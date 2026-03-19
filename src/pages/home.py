bc7f67b57cc0d3b8f53342ce3d2fe8f8283a8c29
Arcee <arceetaraguajuan@gmail.com>
2026-03-19 10:32:04 +0800

    changed graph titles

--- src/pages/home.py
+++ src/pages/home.py
@@ -173,7 +173,7 @@ def render():
             st.error(str(e))
 
     with col2:
-        st.markdown("<h3>Top 10 Countries by Active LEO Satellites</h3>", unsafe_allow_html=True)
+        st.markdown("<h3>Top Countries by Active LEO Satellites</h3>", unsafe_allow_html=True)
 
         try:
             labels, values, error = get_active_leo_by_country()
